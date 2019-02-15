b117ff4b6f153841c0b74090b8b258260ca33dfa
mrewers <marek.rewers@blueservices.pl>
2019-02-15 17:18:36 +0100

    Initial commit

---
+++ cards/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Card(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='static/images/cards/', default='/static/images/cards/default.jpg')
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name_plural = "Cards"
---
+++ cards/urls.py
@@ -0,0 +1,11 @@
+from rest_framework import routers
+from django.urls import path, include
+from .views import CardView
+
+
+router = routers.DefaultRouter()
+router.register(r'', CardView)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
